429c14bac481a536dcfd0f593a03fd39d4831f4c
永田大祐 <dbank0208@gmail.com>
2016-12-20 13:45:14 +0900

    update

---
+++ BLEView/Classes/blText.swift
@@ -0,0 +1,216 @@
+//
+//  blText.swift
+//  Pods
+//
+//  Created by 永田大祐 on 2016/12/11.
+//
+//
+
+import Foundation
+import CoreBluetooth
+
+class blText:NSObject,CBCentralManagerDelegate,CBPeripheralDelegate,CBPeripheralManagerDelegate {
+    
+    var characteristicMutable: CBMutableCharacteristic!
+    var centralManager:CBCentralManager!
+    var peripheral:CBPeripheral!
+    var characteristic:CBCharacteristic!
+    let serviceUUID = CBUUID(string: "DD9B8295-E177-4F8A-A5E1-DC5FED19556D")
+    var peripheralManager: CBPeripheralManager!
+    var characteristicCBC:CBMutableCharacteristic!
+
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager,
+                                  willRestoreState dict: [String : Any]) {
+        // ペリフェラルマネージャ初期化
+        let option : Dictionary =  [
+            CBCentralManagerRestoredStatePeripheralsKey: "dddaisuke"
+        ]
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil , options:option)
+        print("Peripheral Manager Restored")
+    }
+     func centralManager(_ central: CBCentralManager, willRestoreState dict: [String : Any]){
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+
+        print("Central Manager Restored")
+    }
+     func  centralManagerDidUpdateState(_ central:CBCentralManager){
+        print("CentralManagerDidUpdateState", central.state)
+    }
+    //接続開始
+    public  func pushStart(){
+        // ペリフェラルマネージャ初期化
+        let option : Dictionary =  [
+            CBCentralManagerRestoredStatePeripheralsKey: "dddaisuke"
+        ]
+        self.centralManager.connect(peripheral, options: option)
+    }
+    
+    //接続解除
+    public func pushCut(){
+        
+        print("接続カット！")
+        self.centralManager.cancelPeripheralConnection(peripheral)
+        
+    }
+    
+    public func centralManager(_ central: CBCentralManager,
+                               didConnect peripheral: CBPeripheral){
+        
+        print("接続成功！")
+        peripheral.delegate = self
+        //サービス探索開始
+        peripheral.discoverServices(nil)
+    }
+    
+    func centralManager(_ central:CBCentralManager,peripheral:CBPeripheral,error: NSError!){
+        
+        print("接続失敗！")
+        //スキャン開始
+        self.centralManager.scanForPeripherals(withServices: [serviceUUID], options: nil)
+        
+    }
+    
+    // サービス発見時に呼ばれる
+    public func peripheral(_ peripheral : CBPeripheral, didDiscoverServices error : Error?){
+        
+        if let error = error {
+            print("エラー\(error)")
+            return
+        }
+        
+        let services = peripheral.services as NSArray!
+        print("\(services?.count)サービスを発見",services!.count, services!)
+        
+        for  service in services! {
+            
+            // キャラクタリスティック探索開始
+            peripheral.discoverCharacteristics(nil, for: service as! CBService)
+        }
+    }
+    
+    // キャラクタリスティック発見時に呼ばれる
+    public func peripheral(_ peripheral: CBPeripheral,
+                           didDiscoverCharacteristicsFor service: CBService,
+                           error: Error?){
+        if let error = error {
+            print("エラー\(error)")
+            return
+        }
+        
+        let characteristics  = service.characteristics
+        print(" \(characteristics?.count)個のキャラクタリスティックを発見!",characteristics!.count, characteristics!)
+      
+        
+        // 特定のキャラクタリスティックをプロパティに保持
+        let uuid = CBUUID(string:"45088E4B-B847-4E20-ACD7-0BEA181075C2")
+        
+        for aCharacteristic:CBCharacteristic in characteristics! {
+            if aCharacteristic.uuid.isEqual(uuid) {
+                
+                self.characteristic = aCharacteristic
+                break;
+            }
+        }
+    }
+    
+    public func  peripheral (_ peripheral: CBPeripheral,
+                             didUpdateValueFor characteristic: CBCharacteristic,
+                             error: Error?) {
+        if let error = error {
+            
+            print("Read失敗", error, characteristic.uuid)
+            return
+            
+        }
+        
+        print("Read成功",characteristic.service.uuid, characteristic.uuid, characteristic.value!)
+        let serString = String(data: characteristic.value!,encoding: String.Encoding.utf8)
+          }
+    
+    public func peripheral(_ peripheral: CBPeripheral,
+                           didWriteValueFor characteristic: CBCharacteristic,
+                           error: Error?){
+        print("Write成功!")
+    }
+    
+    func publishservice () {
+        
+        // サービスを作成
+        let service = CBMutableService(type: serviceUUID, primary: true)
+        let characteristicUUID = CBUUID(string: "45088E4B-B847-4E20-ACD7-0BEA181075C2")
+        let properties: CBCharacteristicProperties = [.read, .write]
+        let permissions: CBAttributePermissions = [.readable, .writeable]
+        
+        characteristicCBC = CBMutableCharacteristic(
+            type: characteristicUUID,
+            properties: properties,
+            value: nil,
+            permissions: permissions)
+        
+        // キャラクタリスティックをサービスにセット
+        service.characteristics = [characteristicCBC]
+        peripheralManager.add(service)
+        
+    }
+    
+    func startAdvertise() {
+        // アドバタイズメントデータを作成する
+        let advertisementData = [
+            CBAdvertisementDataLocalNameKey: "Test Device",
+            CBAdvertisementDataServiceUUIDsKey: [serviceUUID]
+            ] as [String : Any]
+        
+        // アドバタイズ開始
+        peripheralManager.startAdvertising(advertisementData)
+        
+    }
+    
+    func stopAdvertise () {
+        
+        // アドバタイズ停止
+        peripheralManager.stopAdvertising()
+        
+    }
+    
+    // ペリフェラルマネージャの状態が変化すると呼ばれる
+    public func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        print("state: \(peripheral.state)")
+        
+        switch peripheral.state {
+        case .poweredOn:
+            
+            // サービス登録開始
+            publishservice()
+            
+        default:
+            break
+        }
+    }
+    
+    // サービス追加処理が完了すると呼ばれる
+    public func peripheralManager(_ peripheral: CBPeripheralManager, didAdd service: CBService, error: Error?) {
+        if let error = error {
+            
+            print("サービス追加失敗！ error: \(error)")
+            return
+        }
+        print("サービス追加成功！")
+        
+        // アドバタイズ開始
+        startAdvertise()
+    }
+    
+    // アドバタイズ開始処理が完了すると呼ばれる
+    public func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
+        if let error = error {
+            
+            print("アドバタイズ開始失敗！ error: \(error)")
+            return
+        }
+        print("アドバタイズ開始成功！")
+        //スキャン開始
+        self.centralManager.scanForPeripherals(withServices: [serviceUUID], options: nil)
+    }
+    
+}
